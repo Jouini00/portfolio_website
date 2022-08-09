edd105c9e7eff7f088bad61a3c598ab48f3d0ab5
Jouini00 <h.jouini01@gmail.com>
2022-08-09 12:24:28 -0400

    updated web address

--- app.py
+++ app.py
@@ -24,7 +24,6 @@ class Contact(db.Model):
 def my_home():
     return render_template('index.html')
 
-
 @app.route('/<string:page_name>')
 def html_page(page_name):
     return render_template(page_name)
